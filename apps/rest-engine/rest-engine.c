62fd2c3cb79c4c98b61b444ee080c40fe11ca8d3
cpr4t3s <cpr4t3s@gmail.com>
2017-09-15 02:23:34 +0100

    Another debug disabled

--- apps/rest-engine/rest-engine.c
+++ apps/rest-engine/rest-engine.c
@@ -47,7 +47,7 @@ struct rest_implementation *_rest_interface[2];
 int rest_engine_if_selector = 0;
 
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
